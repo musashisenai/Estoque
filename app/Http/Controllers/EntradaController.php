30be48d46f029a77354daf71e51b09da44b6fc21
musashisenai <musashisenai@gmail.com>
2026-01-27 15:45:05 -0300

    update entradacontroller

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entrada;
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class EntradaController extends Controller
 {
     public function store(Request $request)
     {
+         $produto = Produto::find($request->id_produto);
+
+         if(!$produto) {
+            return response()->json(['message'=>'Produto não encontrado']);
+         }
+
+         if(isset($entrada)){
+            $produto->estoque += $entrada->quantidade;
+         }
+         
+         $produto-> update();
+
         $entrada = Entrada::Create([
             'id_produto' => $request-> id_produto,
             'quantidade' => $request-> quantidade
@@ -28,9 +41,21 @@ class EntradaController extends Controller
     {
         $entrada = Entrada::find($id);
 
-        if(!$entrada) {
+        if($entrada == null) {
             return response()-> json(['message'=>'Produto não encontrado']);
         }
+
+        $produto = $entrada->id_produto;
+
+        $quantidade = $entrada->quantidade;
+
+        $produto = Produto::find($produto);
+
+        if(isset($produto)) {
+            $produto-> estoque -= $quantidade;
+        }
+
+        $produto->update();
 
         $entrada -> delete($id);
 
