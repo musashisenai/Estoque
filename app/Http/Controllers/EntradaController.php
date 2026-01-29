6b991e47cff3ca19e1b97174ec97a91e57fa11ff
musashisenai <musashisenai@gmail.com>
2026-01-29 10:59:20 -0300

    update saida add bloco 6

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -8,57 +8,55 @@ use Illuminate\Http\Request;
 
 class EntradaController extends Controller
 {
-    public function store(Request $request)
-    {
-         $produto = Produto::find($request->id_produto);
-
-         if(!$produto) {
-            return response()->json(['message'=>'Produto não encontrado']);
-         }
-
-         if(isset($entrada)){
-            $produto->estoque += $entrada->quantidade;
-         }
-         
-         $produto-> update();
-
-        $entrada = Entrada::Create([
-            'id_produto' => $request-> id_produto,
-            'quantidade' => $request-> quantidade
-
+    public function store(Request $request){
+    $produto = Produto::find($request->id_produto);  
+    
+    if(!$produto){
+        return response()-> json(['message'=> 'Produto não encontrado']);
+    } else {
+        $entrada = Entrada::create([
+        'id_produto'=> $request-> id_produto,
+        'quantidade'=> $request-> quantidade
         ]);
 
-        return response()->json($entrada);
     }
 
-    public function index() {
+    if(isset($entrada)){
+        $produto-> quantidade_estoque += $entrada->quantidade;
+    }
+    
+    $produto-> update();
+        return response()-> json($entrada);
+        
+    }
+
+    public function index(){
         $entrada = Entrada::all();
 
-        return response()->json($entrada);
+        return response()-> json($entrada);
     }
 
-    public function delete($id)
-    {
+    public function delete($id){
         $entrada = Entrada::find($id);
 
-        if($entrada == null) {
-            return response()-> json(['message'=>'Produto não encontrado']);
+        if($entrada == null){
+            return response()-> json(['message'=> 'Produto não encontrado']);
         }
 
-        $produto = $entrada->id_produto;
+        $produto = $entrada-> id_produto;
 
-        $quantidade = $entrada->quantidade;
+        $quantidade= $entrada-> quantidade;
 
         $produto = Produto::find($produto);
 
-        if(isset($produto)) {
-            $produto-> estoque -= $quantidade;
+        if(isset($produto)){
+            $produto-> quantidade_estoque -= $quantidade;
         }
 
-        $produto->update();
+        $produto-> update();
 
-        $entrada -> delete($id);
+        $entrada-> delete($id);
 
-        return response()->json(['message'=>'Produto deletado com sucesso']);
+        return response()-> json(['Produto deletado']);
     }
 }
--- app/Http/Controllers/SaidaController.php
+++ app/Http/Controllers/SaidaController.php
@@ -2,36 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
+use App\Models\Produto;
 use App\Models\Saida;
 use Illuminate\Http\Request;
 
 class SaidaController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+
+        $cliente = Cliente::find($request->id_cliente);
+        $produto = Produto::find($request->id_produto);
+
+        if ($cliente->idade < $produto->faixa_etaria_minima) {
+            return response()->json(["message" => "Idade insuficiente"]);
+        }
+
         $saida = Saida::Create([
             'id_cliente' => $request->id_cliente,
             'id_produto' => $request->id_produto,
             'quantidade' => $request->quantidade
         ]);
 
+        $produto->quantidade_estoque -= $saida->quantidade;
+
+        $produto->update();
         return response()->json($saida);
+
     }
 
-    public function index(){
+    public function index()
+    {
         $saida = Saida::all();
 
         return response()->json($saida);
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $saida = Saida::find($id);
 
-        if(!$saida){
-            return response()->json(['message'=>'Produto não encontrado']);
+        if (!$saida) {
+            return response()->json(['message' => 'Produto não encontrado']);
         }
 
         $saida->delete($id);
 
-        return response()->json(['message'=>'Produto deletado com sucesso']);
+        return response()->json(['message' => 'Produto deletado com sucesso']);
     }
 }
